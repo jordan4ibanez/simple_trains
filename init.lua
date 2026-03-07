4f14a2c47baa32e1f7085402aa9623cacb7919a9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-06 21:52:47 -0500

    Current position

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ end
 local train = {}
 train.object = nil
 
-train.last_position = vector.new(0, 0, 0)
+train.position = vector.new(0, 0, 0)
 
 train.initial_properties = {
 	visual = "cube",
@@ -25,7 +25,9 @@ end
 function train:on_activate(staticdata, dtime_s)
 	-- self.object:set_acceleration(vector.new(0,-10,0))
 	self.object:set_velocity(vector.new(0, 0, 0))
-	self.last_position = vector.round(self.object:get_pos())
+
+
+	self.position = vector.round(self.object:get_pos())
 end
 
 function train:on_step(dtime, moveresult)
