0c63ea4e2561133dbd272849fe68402416448aee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-06 21:12:10 -0500

    Set up some basic things

--- init.lua
+++ init.lua
@@ -1,4 +1,16 @@
+---@class Train
 local train = {}
+train.object = nil
+
+train.initial_properties = {
+	visual = "cube",
+	physical = true,
+}
+
+function train:on_activate(staticdata, dtime_s)
+	-- self.object:set_acceleration(vector.new(0,-10,0))
+	self.object:set_velocity(vector.new(0,0,0))
+end
 
 function train:test()
 	print("choo choo")
