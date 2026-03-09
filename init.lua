f749dd1f05dbf658ec215e8e942373561082dc5e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-08 20:30:54 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -40,7 +40,7 @@ train.initial_properties = {
 }
 
 function train:on_activate(staticdata, dtime_s)
-	-- self.object:set_acceleration(vector.new(0,-10,0))
+	self.object:set_acceleration(vector.new(0, -10, 0))
 	-- self.object:set_velocity(vector.new(0, 0, 0))
 end
 
