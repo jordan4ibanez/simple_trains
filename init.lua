aefedc854bc367ff72a841077774cb89f5a34ea2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-11 15:35:54 -0400

    Nullify direction

--- init.lua
+++ init.lua
@@ -185,6 +185,9 @@ end
 function train:detect_on_track()
 	self.was_on_track = self.on_track
 	self.on_track = core.get_node(self.position).name == track
+	if not self.on_track  then
+		self.direction = DIRECTION.null
+	end
 end
 
 ---Ensure the forward position still exists.
