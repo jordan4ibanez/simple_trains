1942298ce22aeb4f3a98668f3100a9ee240301b3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-13 16:45:33 -0400

    Testing a parameter in the entity

--- source/init.ts
+++ source/init.ts
@@ -1,1 +1,23 @@
-print("test")
+import { Entity, registerEntity } from "./utility/entity";
+
+class TestTrain extends Entity {
+	on_step(delta: number, moveResult: MoveResult | null): void {
+		for (const ent of core.get_objects_inside_radius(
+			this.object.get_pos(),
+			2,
+		)) {
+			if (ent.is_player()) {
+				continue;
+			}
+
+			const luaEnt = ent.get_luaentity();
+			if (luaEnt == null) {
+				continue;
+			}
+
+			print(luaEnt.name);
+		}
+	}
+}
+
+registerEntity("simple_trains:train", TestTrain);
