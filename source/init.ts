78c9b13e35e98efe409f72f32d8dc6801e80e862
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-13 22:45:23 -0400

    Update init.ts

--- source/init.ts
+++ source/init.ts
@@ -92,21 +92,7 @@ class TestTrain extends Entity {
 	}
 
 	on_step(delta: number, moveResult: MoveResult | null): void {
-		for (const ent of core.get_objects_inside_radius(
-			this.object.get_pos(),
-			2,
-		)) {
-			if (ent.is_player()) {
-				continue;
-			}
-
-			const luaEnt = ent.get_luaentity();
-			if (luaEnt == null) {
-				continue;
-			}
-
-			print(luaEnt.name);
-		}
+		
 	}
 
 	reverseDirection(): void {
