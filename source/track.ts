29faf7c4cafe5ab5dba6c3b6bf07d03c118711ff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-17 18:07:49 -0400

    Update track.ts

--- source/track.ts
+++ source/track.ts
@@ -16,6 +16,9 @@ core.register_node("simple_trains:track_straight", {
 		type: Nodeboxtype.fixed,
 		fixed: [-0.5, -0.5, -0.5, 0.5, -0.3, 0.5],
 	},
+	groups: {
+		oddly_breakable_by_hand: 1,
+	},
 });
 
 export const trackStraightID = core.get_content_id(
