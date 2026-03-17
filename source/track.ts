de66374d4cd6103549d4ded4c2ebe27d18b2ce16
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-17 17:30:03 -0400

    Add in track straight

--- source/track.ts
+++ source/track.ts
@@ -1,1 +1,11 @@
+import { Drawtype, ParamType1, ParamType2 } from "./utility/enums";
+
 export function trackRegistration() {}
+
+core.register_node("simple_trains:track_straight", {
+	paramtype: ParamType1.light,
+	paramtype2: ParamType2["4dir"],
+	drawtype: Drawtype.mesh,
+	mesh: "track_straight.gltf",
+	tiles: ["track_straight.png"],
+});
