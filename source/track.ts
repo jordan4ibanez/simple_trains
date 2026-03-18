a91e11f2b9471d5ae500df356d87f1e9cae6d93f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-17 22:52:36 -0400

    Update track.ts

--- source/track.ts
+++ source/track.ts
@@ -21,6 +21,25 @@ core.register_node("simple_trains:track_straight", {
 	},
 });
 
+core.register_node("simple_trains:track_turn", {
+	paramtype: ParamType1.light,
+	paramtype2: ParamType2["4dir"],
+	drawtype: Drawtype.mesh,
+	mesh: "track_turn.gltf",
+	tiles: ["track_turn.png"],
+	collision_box: {
+		type: Nodeboxtype.fixed,
+		fixed: [-0.5, -0.5, -0.5, 0.5, -0.3, 0.5],
+	},
+	selection_box: {
+		type: Nodeboxtype.fixed,
+		fixed: [-0.5, -0.5, -0.5, 0.5, -0.3, 0.5],
+	},
+	groups: {
+		oddly_breakable_by_hand: 1,
+	},
+});
+
 export const trackStraightID = core.get_content_id(
 	"simple_trains:track_straight",
 );
