bbf01403d2a85a3f43a756a3f2c37ba2f120f2ba
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-24 19:53:20 -0400

    Update init.ts

--- source/init.ts
+++ source/init.ts
@@ -45,16 +45,16 @@ class TurnResult {
 	}
 }
 
-enum SLOP_CLINATION {
+enum SLOPE_CLINATION {
 	up = 0,
 	down = 1,
 }
 class SlopeResult {
 	success: boolean;
 	direction: DIRECTION;
-	clination: SLOP_CLINATION;
+	clination: SLOPE_CLINATION;
 
-	constructor(suc: boolean, dir: DIRECTION, cli: SLOP_CLINATION) {
+	constructor(suc: boolean, dir: DIRECTION, cli: SLOPE_CLINATION) {
 		this.success = suc;
 		this.direction = dir;
 		this.clination = cli;
