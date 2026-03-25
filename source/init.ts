d88ad31479fe876bcea1937ecda798d6cebe8779
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-24 21:52:07 -0400

    Update init.ts

--- source/init.ts
+++ source/init.ts
@@ -358,7 +358,7 @@ class TestTrain extends Entity {
 			this.slope = slopeCheck(checker, this.direction);
 			this.setSlope();
 		} else if (oldMove > 0.5 && newMove <= 0.5) {
-			// print("forward (moving backward)");
+			print("forward (moving backward)");
 			// In front of train position.
 			// Inverse of direction.
 			const checker = new Vec3().setVec(this.position);
@@ -382,6 +382,13 @@ class TestTrain extends Entity {
 			// print("backward (moving forward)");
 			// Behind train position.
 			// Direction.
+			const checker = new Vec3().setVec(this.position);
+
+			this.slope = swapTrainSlope(
+				slopeCheck(checker, directionInversion[this.direction]),
+			);
+
+			this.setSlope();
 		}
 
 		if (updateCheck) {
