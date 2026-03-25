538e755137743e5589cdbac7739448658297c102
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-03-25 19:11:03 -0400

    Update init.ts

--- source/init.ts
+++ source/init.ts
@@ -503,35 +503,36 @@ class TestTrain extends Entity {
 	}
 
 	update(updateCheck: boolean): void {
-		if (updateCheck) {
-			// print("update check");
-			if (this.movement > 0) {
-				if (!this.continueStraight(false).success) {
-					const result = this.turn();
-					if (result.success) {
-						// The train can continue.
-						this.direction = result.direction;
-						this.setRotation();
-					} else {
-						// The train has hit a path end.
-						this.movement = 0;
-						this.speed = 0;
-						// todo: reset the slope setting! If it hit a path end it cannot be on a slope
-					}
+		if (!updateCheck) {
+			return;
+		}
+		// print("update check");
+		if (this.movement > 0) {
+			if (!this.continueStraight(false).success) {
+				const result = this.turn();
+				if (result.success) {
+					// The train can continue.
+					this.direction = result.direction;
+					this.setRotation();
+				} else {
+					// The train has hit a path end.
+					this.movement = 0;
+					this.speed = 0;
+					// todo: reset the slope setting! If it hit a path end it cannot be on a slope
 				}
-			} else {
-				if (!this.continueStraight(true).success) {
-					const result = this.turn();
-					if (result.success) {
-						// The train can continue.
-						this.direction = directionInversion[result.direction];
-						this.setRotation();
-					} else {
-						// The train has hit a path end.
-						this.movement = 0;
-						this.speed = 0;
-						// todo: reset the slope setting! If it hit a path end it cannot be on a slope
-					}
+			}
+		} else {
+			if (!this.continueStraight(true).success) {
+				const result = this.turn();
+				if (result.success) {
+					// The train can continue.
+					this.direction = directionInversion[result.direction];
+					this.setRotation();
+				} else {
+					// The train has hit a path end.
+					this.movement = 0;
+					this.speed = 0;
+					// todo: reset the slope setting! If it hit a path end it cannot be on a slope
 				}
 			}
 		}
